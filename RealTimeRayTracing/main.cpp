1a849fba6a33caa46fcebe6b51404b2e7c4f0702
EngxDANOx <daniel8brown@hotmail.co.uk>
2019-12-30 20:00:53 +0000

    now works in linux! also added some code to show device info

--- RealTimeRayTracing/main.cpp
+++ RealTimeRayTracing/main.cpp
@@ -1,10 +1,45 @@
-#pragma warning(disable : 4996)//im not using the deprecited functions
+#ifdef _WIN32
+//#pragma warning(disable : 4996)//im not using the deprecited functions
+#endif
 #include <CL/cl.hpp>
+#include <iostream>
 
 int main()
 {
-	//cl::Platro
-	cl::Platform platform;
+	std::vector<cl::Platform> platforms;
+	cl::Platform::get(&platforms);
+
+	if(platforms.size() <= 0)
+	{
+		std::cerr << "platforms is empty" << std::endl;
+		return -1;
+	}
+
+	std::cout << "num platforms\t" << platforms.size() << "\n";
+
+	auto platform = platforms.front();
+	std::vector<cl::Device> devices;
+	platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+	if(devices.size() <= 0)
+	{
+		std::cerr << "devices is empty" << std::endl;
+		return -1;
+	}
+
+	std::cout << "num devices\t\t" << devices.size() << "\n\n";
+
+	//auto device = devices.front();
+	int i = 1;
+	for(auto device = devices.begin(); device != devices.end(); device++, i++)
+	{
+		auto vendor = device->getInfo<CL_DEVICE_VENDOR>();
+		auto version = device->getInfo<CL_DEVICE_VERSION>();
+
+		std::cout << "Device: \t" << i << "\n";
+		std::cout << "Vendor: \t" << vendor << "\n";
+		std::cout << "Version:\t" << version << "\n\n";
+	}
 
 	return 0;
 }
