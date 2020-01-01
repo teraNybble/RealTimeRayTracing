5ad40472c2b2c8433e5176cab56425a91c65ce44
EngxDANOx <daniel8brown@hotmail.co.uk>
2020-01-01 17:26:45 +0000

    small changes and some optamisations (I think)

--- RealTimeRayTracing/main.cpp
+++ RealTimeRayTracing/main.cpp
@@ -11,43 +11,6 @@ void printDeviceInfo(const std::vector<cl::Device>& devices);
 
 int main()
 {
-	/*
-	std::vector<cl::Platform> platforms;
-	std::vector<cl::Device> devices;
-
-	initDevices(platforms, devices, CL_DEVICE_TYPE_GPU);
-
-	auto device = devices.front();
-
-	//open the .cl file
-	std::ifstream helloWorldFile("CLfiles/helloWorld.cl");
-	//store the contence of the file in a string
-	std::string src = std::string(std::istreambuf_iterator<char>(helloWorldFile),(std::istreambuf_iterator<char>()));
-	cl::Program::Sources sources(1,std::make_pair(src.c_str(), src.length() + 1));
-
-	cl::Context context(device);
-
-	cl::Program program(context, sources);
-	//build the program
-	auto err = program.build("-cl-std=CL1.2");
-	//print out any errors if they occur
-	if(err != 0) { std::cerr << "OpenCL error: " << err << "\n"; }
-
-	char buf[16];
-	cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
-	cl::Kernel kernel(program, "HelloWorld", &err);
-
-	if(err != 0) { std::cerr << "OpenCL error: " << err << "\n"; }
-
-	kernel.setArg(0, memBuf);
-
-	cl::CommandQueue queue(context, device);
-	queue.enqueueTask(kernel);
-	queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, sizeof(buf), buf);
-
-	std::cout << buf;
-*/
-
 	cl::Program program = createProgram("CLfiles/helloWorld.cl");
 	cl::Context context = program.getInfo<CL_PROGRAM_CONTEXT>();
 	cl::Device device = context.getInfo<CL_CONTEXT_DEVICES>().front();
@@ -69,6 +32,7 @@ int main()
 
 void initDevices(std::vector<cl::Platform>& platforms, std::vector<cl::Device>& devices, int deviceType)
 {
+#pragma unroll
 	for(int i = 0; i < 40; i++)
 		std::cout << "-";
 	std::cout << "\n";
@@ -106,7 +70,7 @@ void initDevices(std::vector<cl::Platform>& platforms, std::vector<cl::Device>& 
 		std::cout << "Vendor: \t" << vendor << "\n";
 		std::cout << "Version:\t" << version << "\n\n";
 	}
-
+#pragma unroll
 	for(int i = 0; i < 40; i++)
 		std::cout << "-";
 	std::cout << "\n";
@@ -139,11 +103,7 @@ cl::Program createProgram(const std::string& file)
 	cl::Platform::get(&platforms);
 
 	//if the platforms are empty there is no point to running the code
-	if(platforms.empty())
-	{
-		std::cerr << "platforms is empty" << std::endl;
-		exit(-1);
-	}
+	if(platforms.empty()) { std::cerr << "platforms is empty" << std::endl;	exit(1); }
 	std::cout << "num platforms\t" << platforms.size() << "\n";
 
 	auto platform = platforms.front();
@@ -152,11 +112,7 @@ cl::Program createProgram(const std::string& file)
 	auto device = devices.front();
 
 	//if there are no devices then we can't use OpenCL
-	if(devices.empty())
-	{
-		std::cerr << "devices is empty" << std::endl;
-		exit(-1);
-	}
+	if(devices.empty()) { std::cerr << "devices is empty" << std::endl;	exit(2); }
 
 	printDeviceInfo(devices);
 
@@ -174,7 +130,7 @@ cl::Program createProgram(const std::string& file)
 
 	auto error = program.build("-cl-std=CL1.2");
 
-	if(error) { std::cerr << "OpenCL error:\t" << error << std::endl; }
+	if(error) { std::cerr << "OpenCL error:\t" << error << std::endl; exit(error); }
 
 	return program;
 }
